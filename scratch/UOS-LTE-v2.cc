b0a8c1d311a9fd4c4c569f94a47a6af781e85982
Hao123br <carlosh2340@gmail.com>
2020-03-15 16:37:27 -0300

    Ajust loss model parameter

--- scratch/UOS-LTE-v2.cc
+++ scratch/UOS-LTE-v2.cc
@@ -1475,7 +1475,7 @@ NodeContainer ueNodes;
 	    
 			//modelFactory.SetTypeId (Cost231PropagationLossModel::GetTypeId());
 			NS_LOG_UNCOND("Pathloss model: HybridBuildingsPropagationLossModel ");
-			Config::SetDefault("ns3::ItuR1411NlosOverRooftopPropagationLossModel::StreetsOrientation", DoubleValue (10));
+			Config::SetDefault("ns3::ItuR1411NlosOverRooftopPropagationLossModel::StreetsOrientation", DoubleValue (0));
 			lteHelper->SetAttribute ("PathlossModel", StringValue ("ns3::HybridBuildingsPropagationLossModel"));
 			lteHelper->SetPathlossModelAttribute ("Frequency", DoubleValue (1.8e9));
 			//lteHelper->SetPathlossModelAttribute ("Environment", EnumValue (EnvironmentType::OpenAreasEnvironment));
