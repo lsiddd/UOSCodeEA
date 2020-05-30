3c86f5d1387821ca16e52bcf49522e305ae688ff
Hao123br <carlosh2340@gmail.com>
2020-05-30 20:58:31 -0300

    SEM script: Add option to delete phystats trace files

--- run-simulations.py
+++ run-simulations.py
@@ -1,5 +1,6 @@
 import sem
 import argparse
+import os
 
 ns_path = './'
 script = 'UOS-LTE-v2'
@@ -11,6 +12,8 @@ parser.add_argument('-o', '--overwrite', action='store_true',
                     help='Overwrite previous campaign')
 parser.add_argument('-s', '--save', action='store_true',
                     help="Don't run simulations, just save the results")
+parser.add_argument('--no-traces', action='store_true',
+                    help="Delete phystats trace files")
 args = parser.parse_args()
 
 campaign = sem.CampaignManager.new(ns_path, script, campaign_dir,
@@ -54,3 +57,6 @@ if not args.save:
     campaign.run_missing_simulations(sem.list_param_combinations(param_combinations),33)
 
 campaign.save_to_folders(result_param, results_dir, 33)
+
+if args.no_traces:
+    os.system("find " + results_dir + " -name UlInterferenceStats.txt -delete")
